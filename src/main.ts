7fc266812ef6def48f32a9dccf0b95a19633c161
catanuclei <catanuclei@proton.me>
2024-12-25 19:02:04 +0100

    Base desktop element

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
 import '@styles/reset.scss';
 import '@styles/font.scss';
+import '@styles/core.scss';
+
+const desktopEl = document.getElementById('desktop')!;
+const windowManagerEl = document.getElementById('window-manager')!;
+
+console.log(desktopEl);
+console.log(windowManagerEl);
