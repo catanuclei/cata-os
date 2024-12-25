1333724a9c67b3e5fc4d251108f023470f44fc9b
catanuclei <catanuclei@proton.me>
2024-12-25 20:05:20 +0100

    Base window manager logic

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { initializeDesktopInfo } from '@utils/setup';
+import { createWindow } from '@utils/window';
 
 import '@styles/reset.scss';
 import '@styles/font.scss';
 import '@styles/core.scss';
 
-const desktopEl = document.getElementById('desktop')!;
-const windowManagerEl = document.getElementById('window-manager')!;
+export const desktopEl = document.getElementById('desktop')!;
+export const windowManagerEl = document.getElementById('window-manager')!;
 
 initializeDesktopInfo(desktopEl.querySelector('.desktop__info')!);
+createWindow('Window 1');
+createWindow('Window 2');
+createWindow('Window 3');
 
 console.log(desktopEl);
 console.log(windowManagerEl);
