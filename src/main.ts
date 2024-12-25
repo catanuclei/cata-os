f54282a8f6eed98af6dfcc78ed9cc3c872dae34f
catanuclei <catanuclei@proton.me>
2024-12-25 19:08:46 +0100

    Initializing desktop info

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { initializeDesktopInfo } from '@utils/setup';
+
 import '@styles/reset.scss';
 import '@styles/font.scss';
 import '@styles/core.scss';
 
 const desktopEl = document.getElementById('desktop')!;
 const windowManagerEl = document.getElementById('window-manager')!;
+
+initializeDesktopInfo(desktopEl.querySelector('.desktop__info')!);
 
 console.log(desktopEl);
 console.log(windowManagerEl);
---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+export const OsConstants = {
+  DISPLAY_NAME: 'CataOS',
+  VERSION: '0.0.1',
+  AUTHOR: 'catanuclei',
+  COPYRIGHT_YEAR: '2024',
+};
