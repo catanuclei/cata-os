ff15cbddea3b146d0c790d370066be13fd037404
catanuclei <catanuclei@proton.me>
2024-12-26 15:23:11 +0100

    Generating window keys

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,17 @@ export const OsConstants = {
   AUTHOR: 'catanuclei',
   COPYRIGHT_YEAR: '2024',
 };
+
+export const KEY_CHARACTERS = (() => {
+  const characters: string[] = [];
+  for (let i = 0; i < 10; i++) {
+    characters.push(i.toString());
+  }
+  for (let i = 0; i < 26; i++) {
+    characters.push(String.fromCharCode(97 + i));
+  }
+  for (let i = 0; i < 26; i++) {
+    characters.push(String.fromCharCode(65 + i));
+  }
+  return characters;
+})();
--- src/utils/window.ts
+++ src/utils/window.ts
@@ -1,8 +1,19 @@
 import { createIcon } from './icon';
 
+import { KEY_CHARACTERS } from './constants';
+
+const WINDOW_CLASS = 'window';
+const WINDOW_TITLE_CLASS = 'window__title';
+const WINDOW_KEY_LENGTH = 6;
+
 interface WindowInfo {
+  title: string;
+}
+
+interface WindowResponse {
   key: string;
 }
+
 export class WindowManager {
   private _managerNode: HTMLElement;
 
@@ -10,17 +21,32 @@ export class WindowManager {
     this._managerNode = managerNode;
   }
 
-  public createWindow = (title: string): WindowInfo => {
-    const key = 'key';
-    const windowNode = document.createElement('div')!;
-    const titleNode = document.createElement('p')!;
-
-    windowNode.classList.add('window');
-    titleNode.innerHTML = title;
-    titleNode.appendChild(createIcon('vr'));
-    windowNode.appendChild(titleNode);
+  public createWindow = (title: string): WindowResponse => {
+    const key = this._generateWindowKey();
+    const windowNode = _createWindowNode({ title });
     this._managerNode.appendChild(windowNode);
-
     return { key };
   };
+
+  private _generateWindowKey = (): string => {
+    let result = '';
+    for (let i = 0; i < WINDOW_KEY_LENGTH; i++) {
+      result +=
+        KEY_CHARACTERS[Math.floor(Math.random() * KEY_CHARACTERS.length)];
+    }
+    return result;
+  };
 }
+
+const _createWindowNode = ({ title }: WindowInfo): HTMLElement => {
+  const node = document.createElement('div')!;
+  const titleNode = document.createElement('p')!;
+
+  node.classList.add(WINDOW_CLASS);
+  titleNode.classList.add(WINDOW_TITLE_CLASS);
+  titleNode.innerHTML = title;
+  titleNode.appendChild(createIcon('x'));
+  node.appendChild(titleNode);
+
+  return node;
+};
