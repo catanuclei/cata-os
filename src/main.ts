39f3832120eee13f194f236f401d5e5b8f3e73be
catanuclei <catanuclei@proton.me>
2024-12-26 15:06:12 +0100

    Adding and setting up Bootstrap Icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { initializeDesktopInfo } from '@utils/setup';
 import { createWindow } from '@utils/window';
 
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+
 import '@styles/reset.scss';
 import '@styles/font.scss';
 import '@styles/core.scss';
@@ -12,6 +14,8 @@ initializeDesktopInfo(desktopEl.querySelector('.desktop__info')!);
 createWindow('Window 1');
 createWindow('Window 2');
 createWindow('Window 3');
+createWindow('Window 4');
+createWindow('Window 5');
 
 console.log(desktopEl);
 console.log(windowManagerEl);
--- src/utils/window.ts
+++ src/utils/window.ts
@@ -1,11 +1,14 @@
 import { windowManagerEl } from '../main';
 
+import { createIcon } from './icon';
+
 export const createWindow = (title: string): void => {
   const windowNode = document.createElement('div')!;
   const titleNode = document.createElement('p')!;
 
   windowNode.classList.add('window');
   titleNode.innerHTML = title;
+  titleNode.appendChild(createIcon('vr'));
   windowNode.appendChild(titleNode);
   windowManagerEl.appendChild(windowNode);
 };
